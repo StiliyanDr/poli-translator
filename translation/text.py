aac1532302cc6616831f3b75d103caeaafca60e6
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-08-07 16:09:10 +0300

    Make Text equality-testable

--- translation/text.py
+++ translation/text.py
@@ -11,3 +11,11 @@ class Text:
         self.body = body
         self.from_language = from_language
         self.to_language = to_language
+
+    def __eq__(self, other):
+        return (
+            type(self) == type(other) and
+            self.body == other.body and
+            self.from_language is other.from_language and
+            self.to_language is other.to_language
+        )
