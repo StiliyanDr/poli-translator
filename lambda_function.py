b95707e2348c47fd68e06221fc7ef04470ba9ace
Stiliyan Dragnev <stiliyan_dragnev@yahoo.com>
2023-08-10 15:37:18 +0300

    Add logging to the lambda handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 
 import translation as tr
@@ -6,8 +7,16 @@ import translation as tr
 translator = tr.Translator(default_to_lang=tr.Language.EN)
 
 
+def log_results(pairs):
+    for i, (text, translation) in enumerate(pairs):
+        logging.info("ITEM ", i + 1)
+        logging.info("Original:", text)
+        logging.info("Translation:", translation)
+
+
 def lambda_handler(event, context):
     translation = tr.lambda_handler(event, translator)
+    log_results(translation)
 
     return {
         "py-version": sys.version_info,
